717c5eacd18903d6cc6eee24c36a318ca939fc58
hitesh <hitesh.arora1997@gmail.com>
2022-09-06 15:47:08 +0200

    Add a flag

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "This document contains all the problem statements in format of `question,answer``")
+	flag.Parse()
+	_ = csvFilename
 }
