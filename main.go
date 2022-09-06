ed299d196e645aee57e771022d8a62a84918e29b
hitesh <hitesh.arora1997@gmail.com>
2022-09-06 15:53:51 +0200

    trying to reading the file

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "This document contains all the problem statements in format of `question,answer``")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Println("file doesn't exist", *csvFilename)
+		os.Exit(1)
+	}
+	_ = file
 }
